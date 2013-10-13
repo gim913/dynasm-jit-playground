84fd6eb784b200453a7990a0a00fd3e00465923a
GiM <gim913@gmail.com>
2013-10-13 16:02:30 +0200

    slicing-by-four example x86 version

--- src/examp2.cpp
+++ src/examp2.cpp
@@ -7,13 +7,46 @@
 #endif
 
 #include <iostream>
+#include <stdint.h>
+
+uint32_t crcTab[4*256];
+
+void crcInit() {
+	for (unsigned int i = 0; i <= 0xFF; i++) {
+		uint32_t crc = i;
+		for (unsigned int j = 0; j < 8; j++) {
+			crc = (crc >> 1) ^ ((crc & 1) * 0x04C11DB7);
+		}
+		crcTab[0 + i] = crc;
+	}
+
+	for (unsigned int i = 0; i <= 0xFF; i++) {
+	  crcTab[1*256 + i] = (crcTab[0*256 + i] >> 8) ^ crcTab[0 + (crcTab[0*256 + i] & 0xFF)];
+	  crcTab[2*256 + i] = (crcTab[1*256 + i] >> 8) ^ crcTab[0 + (crcTab[1*256 + i] & 0xFF)];
+	  crcTab[3*256 + i] = (crcTab[2*256 + i] >> 8) ^ crcTab[0 + (crcTab[2*256 + i] & 0xFF)];
+	}
+}
+
+uint32_t crcSlicing4(const void* data, size_t length, uint32_t previousCrc = 0)
+{
+	uint32_t* current = (uint32_t*) data;
+	uint32_t crc = ~previousCrc;
+
+	while (length >= 4)
+	{
+		crc ^= *current++;
+		crc  =
+			crcTab[3*256 + (crc & 0xFF)] ^
+			crcTab[2*256 + ((crc>>8) & 0xFF)] ^
+			crcTab[1*256 + ((crc>>16) & 0xFF)] ^
+			crcTab[0*256 + (crc>>24)];
+		length -= 4;
+	}
+	return ~crc;
+}
 
 int main(int argc, char *argv[]) {
-	if (argc <= 1) {
-		std::cout << "syntax: examp2 <integer>" << std::endl;
-		return -1;
-	}
-	int num = atoi(argv[1]);
+	crcInit();
 
 #ifdef _WIN64
 	std::cout << "compiled for x64" << std::endl;
@@ -21,17 +54,28 @@ int main(int argc, char *argv[]) {
 	std::cout << "compiled for x86" << std::endl;
 #endif
 
-	try {
-		DynAsm da(actions);
+	const size_t dataSize = 0x100000;
+	void* dataBlock=malloc(dataSize);
+	char* temp=(char*)dataBlock;
+	uint64_t x = 0x416947;
+	for (size_t i=0; i<0x100000; ++i) {
+		x = (x*0x7FFFFFED + 0x7FFFFFC3) % (0x7fffffff);
+		*temp++ = x&0xff;
+	}
 
-		dynasmGenerator(&da, num);
+	try {
+		DynAsm da(actions, GLOB__MAX);
+
+		dynasmGenerator(&da);
   
-		int (*fptr)() = reinterpret_cast<int(*)()>( da.build() );
+		uint32_t (__fastcall * fptr)(void*,size_t) = reinterpret_cast<uint32_t(__fastcall *)(void*, size_t)>( da.build() );
 
 		// Call the JIT-ted function.
-		int ret = fptr();
+		uint32_t real = crcSlicing4(dataBlock, dataSize);
+		uint32_t ret = fptr(dataBlock, dataSize);
 	
 		std::cout << "code returend value: " << ret << std::endl;
+		std::cout << "crc  returend value: " << real << std::endl;
 		std::cout << "last pc val: " << da.getPc(0) << std::endl;
 		da.destroy(fptr);
 
